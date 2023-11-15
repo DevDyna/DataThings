a7c829216d746c6c773ca850701422bb56cbfab4
DevDyna <andrearisorti2003@gmail.com>
2023-11-15 23:03:51 +0100

    method io

---
+++ methods/io.mjs
@@ -0,0 +1,26 @@
+import fs from 'fs';
+//-----------------------------------------------------------------------------------//
+export function JsonArray(url) { //'tags/acacia_door.json'
+    return JSON.parse(fs.readFileSync(url));
+}
+//-----------------------------------------------------------------------------------//
+export function getAllFromDir(url) { //'./tests/'
+    let list = []
+    fs.readdirSync(url).forEach(file => {
+        list.push(file)
+    });
+    return list
+}
+//-----------------------------------------------------------------------------------//
+export function mkFile(url) {
+	let bool = true;
+    try {
+        fs.writeFileSync(url, content);
+    } catch (e) {
+        bool = false //error writing file
+    }
+    return bool
+}
+
+//-----------------------------------------------------------------------------------//
+//-----------------------------------------------------------------------------------//
