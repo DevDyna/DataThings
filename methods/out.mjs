bafca0301716375fb659a4e61238b31e4275c7dd
DevDyna <andrearisorti2003@gmail.com>
2024-11-04 23:14:02 +0100

    Update out.mjs

--- methods/out.mjs
+++ methods/out.mjs
@@ -1,36 +1,31 @@
-import * as log from './logger.mjs';
 //-------------------------------------------------//
-export function out(st,silent) {
-if(!silent)
-log.log(st)
+export function out(st) {
     process.stdout.write(st);
 }
 //-------------------------------------------------//
-export function outln(st,silent) {
-if(!silent)
-log.log(st)
+export function outln(st) {
     console.log(st);
 }
 //-------------------------------------------------//
-export function array_out(ar,ln,silent){
+export function array_out(ar,ln){
 	for(var i=0;i<ar.length;i++){
 		if(ln){
-		outln(ar[i],silent);
+		outln(ar[i]);
 		}else{
-		out(ar[i],silent);
+		out(ar[i]);
 		}
 	}
 }
 //-------------------------------------------------//
-export function matrix_out(mtx,ln,silent) {
+export function matrix_out(mtx,ln) {
     for (var x = 0; x < mtx.length; x++) {
-		out("  ",silent);//console fixer
+		out("  ");//console fixer
         for (var y = 0; y < mtx[0].length; y++) {
 
-            out(mtx[x][y],silent);
+            out(mtx[x][y]);
         }
 		if(ln){
-        out("\n",silent);
+        out("\n");
 		}
     }
 }
