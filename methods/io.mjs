6b98a08440f9df45867d08edf99243339b85ea49
DevDyna <andrearisorti2003@gmail.com>
2024-11-05 13:37:11 +0100

    Update io.mjs

--- methods/io.mjs
+++ methods/io.mjs
@@ -1,51 +1,57 @@
-import fs from 'fs';
+import fs from "fs";
 //-----------------------------------------------------------------------------------//
 /**
- * 
+ *
  * @param {string} url json url
  * @returns array
  */
-export function JsonArray(url) { //'tags/acacia_door.json'
-    return JSON.parse(fs.readFileSync(url));
+export function JsonArray(url) {
+  //'tags/acacia_door.json'
+  return JSON.parse(fs.readFileSync(url));
 }
 //-----------------------------------------------------------------------------------//
-export function getAllFromDir(url) { //'./tests/'
-    let list = []
-    fs.readdirSync(url).forEach(file => {
-        list.push(file)
-    });
-    return list
+export function getAllFromDir(url) {
+  //'./tests/'
+  let list = [];
+  fs.readdirSync(url).forEach((file) => {
+    list.push(file);
+  });
+  return list;
 }
 //-----------------------------------------------------------------------------------//
 /**
- * 
- * @param {string} url 
- * @param {string} content '{data:01}'
+ *
+ * @param {string} url
+ * @param {string} content {data:01} to stringify
  * @returns error
  */
-export function mkFile(url,content) {
-	let bool = true;
-    try {
-        fs.writeFileSync(url,JSON.stringify(content));
-    } catch (e) {
-        console.log(e)
-        bool = false //error writing file
-    }
-    return bool
+export function mkFile(url, content,silent) {
+  let bool = true;
+  if(typeof silent == undefined)silent = false
+  try {
+    fs.writeFileSync(url, JSON.stringify(content, null, 2));
+  } catch (e) {
+    if(silent)
+    console.log(e);
+    bool = false; //error writing file
+  }
+  return bool;
 }
 //-----------------------------------------------------------------------------------//
-export function mkDir(url){
-	let bool = true
-	try{
-	fs.mkdirSync(url)
-	}catch(e){
-        console.log(e)
-		bool = false
-	}
-	return bool
+export function mkDir(url, silent) {
+  let bool = true;
+  if(typeof silent == undefined)silent = false
+  try {
+    fs.mkdirSync(url);
+  } catch (e) {
+    if(silent)
+    console.log(e);
+    bool = false;
+  }
+  return bool;
 }
 //-----------------------------------------------------------------------------------//
-export function isDir(url){
+export function isDir(url) {
   try {
     return fs.statSync(url).isDirectory();
   } catch (error) {
@@ -53,28 +59,26 @@ export function isDir(url){
   }
 }
 //-----------------------------------------------------------------------------------//
-export function rawWrite(url, content){
-    let bool = true
-        try {
-                // Create the file and write the content
-                fs.writeFile(url, content);
-
-        } catch (e) {
-            console.log(e)
-            bool = false //error writing file
-        }
-        return bool
+export function rawWrite(url, content) {
+  let bool = true;
+  try {
+    // Create the file and write the content
+    fs.writeFile(url, content);
+  } catch (e) {
+    console.log(e);
+    bool = false; //error writing file
+  }
+  return bool;
 }
 //-----------------------------------------------------------------------------------//
-export function rawAppend(url, content){
-    let bool = true
-        try {
-                // Append the content to the file
-                fs.appendFileSync(url, content);
-
-        } catch (e) {
-            console.log(e)
-            bool = false //error writing file
-        }
-        return bool
+export function rawAppend(url, content) {
+  let bool = true;
+  try {
+    // Append the content to the file
+    fs.appendFileSync(url, content);
+  } catch (e) {
+    console.log(e);
+    bool = false; //error writing file
+  }
+  return bool;
 }
