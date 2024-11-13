e3fff41dc8701e387c139055e9559b692e256a4e
DevDyna <andrearisorti2003@gmail.com>
2024-11-13 12:55:45 +0100

    Update modmaker.bat

--- modmaker.bat
+++ modmaker.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Creating JAR File
-jar cf "ExtraBounties.jar" data META-INF pack.mcmeta pack.png
+jar cf "ExtraBounties-universal.jar" data META-INF pack.mcmeta pack.png assets
 ECHO JAR Created!
 TIMEOUT /NOBREAK /T 1 > nul
