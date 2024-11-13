38ed93774847591bd4bfc749371bd813e19f6c55
DevDyna <andrearisorti2003@gmail.com>
2024-11-13 12:55:30 +0100

    Update zipmaker.bat

--- zipmaker.bat
+++ zipmaker.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Creating ZIP File
-tar.exe -a -cf "ExtraBounties.zip" data pack.png pack.mcmeta
+tar.exe -a -cf "ExtraBounties-universal.zip" data pack.png pack.mcmeta assets
 ECHO ZIP Created!
 TIMEOUT /NOBREAK /T 1 > nul
