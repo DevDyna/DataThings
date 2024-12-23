b4efbc55e6ef961abdad985a3424a5465d525ae5
DevDyna <andrearisorti2003@gmail.com>
2024-12-23 22:31:43 +0100

    all setup

---
+++ execute.mjs
@@ -0,0 +1,281 @@
+import * as io from "./methods/io.mjs";
+import * as vars from "./gregConst.mjs";
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+//await delay(1000) /// waiting 1 second.	///only if async enable
+
+main();
+
+async function main() {
+  const { veins, colors, files, breakerblocks, breakerReplace } = vars;
+
+  console.log("scanning for directories");
+
+  await delay(500);
+  outputKeys(files);
+
+
+  /**
+   * 
+   * @param {object} obj 
+   * @param {string} path optional 
+   */
+  function outputKeys(obj, path = "") {
+    for (const key in obj) {
+      const newPath = path ? `${path}/${key}` : key;
+      if (io.mkDir("./" + newPath)) {
+        console.log("Directory Created: " + newPath);
+      } else {
+        console.log("Found " + newPath);
+      }
+      if (obj[key] !== null && typeof obj[key] === "object") {
+        outputKeys(obj[key], newPath);
+      }
+    }
+  }
+
+  await delay(500);
+  console.log("Generating All Content");
+
+
+  /**
+   * 
+   * @param {string} item like "minecraft:stone" 
+   * @param {string} customUrl optional | like "./data/base.json"
+   * 
+   */
+  let RockBreakerRecipe = async (item, customUrl) => {
+    console.log("RecipeBuilder : rock_breaker -> " + item);
+    await delay(500);
+
+    io.mkFile(
+      typeof customUrl == "string"
+        ? customUrl
+        : "./data/gtnihilo/recipe/rock_breaker/" +
+            replaceColonWithUnderscore(item) +
+            ".json",
+      {
+        data: { fluidA: "lava", fluidB: "water" },
+        duration: 16,
+        recipeConditions: [{ type: "rock_breaker" }],
+        tickInputs: {
+          eu: [{ content: 16, chance: 10000, max_chance: 10000 }],
+        },
+        type: "gtceu:rock_breaker",
+        inputs: {
+          item: [
+            {
+              content: { ingredient: { item: item }, count: 1 },
+              max_chance: 10000,
+            },
+          ],
+        },
+        outputs: {
+          item: [
+            {
+              content: {
+                ingredient: {
+                  items: item,
+                  components: {
+                    "minecraft:max_stack_size": 64,
+                    "minecraft:attribute_modifiers": { modifiers: [] },
+                    "minecraft:lore": [],
+                    "minecraft:rarity": "common",
+                    "minecraft:enchantments": { levels: {} },
+                    "minecraft:repair_cost": 0,
+                  },
+                  strict: true,
+                  type: "neoforge:components",
+                },
+                count: 1,
+              },
+              chance: 10000,
+              max_chance: 10000,
+            },
+          ],
+        },
+      }
+    );
+  };
+
+  /**
+   * 
+   * @param {string} input like "minecraft:stone" 
+   * @param {object[]} outputs created by using CreateItem
+   */
+  let SifterRecipe = async (input, outputs) => {
+    console.log("RecipeBuilder : sifter -> " + input);
+    await delay(500);
+    io.mkFile(
+      "./data/gtnihilo/recipe/sifter/" +
+        replaceColonWithUnderscore(input) +
+        ".json",
+      {
+        duration: 100,
+        tickInputs: {
+          eu: [{ content: 16, chance: 10000, max_chance: 10000 }],
+        },
+        type: "gtceu:sifter",
+        inputs: {
+          item: [
+            {
+              content: { ingredient: { item: input }, count: 1 },
+              chance: 10000,
+              max_chance: 10000,
+            },
+          ],
+        },
+        outputs: {
+          item: outputs,
+        },
+      }
+    );
+  };
+  /**
+   * 
+   * @param {*} output like "minecraft:stone"
+   * @param {*} chance 0 to 100
+   * @returns 
+   */
+  let CreateItem = (output, chance) => {
+    return {
+      content: {
+        ingredient: {
+          items: output,
+          components: {
+            "minecraft:max_stack_size": 64,
+            "minecraft:attribute_modifiers": { modifiers: [] },
+            "minecraft:lore": [],
+            "minecraft:rarity": "common",
+            "minecraft:enchantments": { levels: {} },
+            "minecraft:repair_cost": 0,
+          },
+          strict: true,
+          type: "neoforge:components",
+        },
+        count: 1,
+      },
+      chance: chance * 100,
+      max_chance: 10000,
+    };
+  };
+  /**
+   * 
+   * @param {string} name
+   * @returns "gtnihilo:"+name
+   */
+  let IDSpecial = (name) => {
+    return "gtnihilo:" + name;
+  };
+  /**
+   * 
+   * @param {string} str 
+   * @returns replace ":" to "_"
+   */
+  let replaceColonWithUnderscore = (str) => {
+    return str.replace(/:/g, "_");
+  };
+  /**
+   * 
+   * @param {string} nameData 
+   * @returns replace "_" to " " keep spaces
+   */
+  let formatNameDataWithSpaces = (nameData) => {
+    return nameData
+      .split("_") // Split by underscores
+      .map((segment) => segment.charAt(0).toUpperCase() + segment.slice(1)) // Capitalize first letter of each segment
+      .join(" "); // Join the segments with spaces
+  };
+
+  // gtceu rock breaker recipe nerfer
+  breakerReplace.forEach((e) => {
+    RockBreakerRecipe(
+      "minecraft:" + e,
+      "./data/gtceu/recipe/rock_breaker/" + e + ".json"
+    );
+  });
+
+  //vein result shifter recipe generator
+  Object.keys(veins).forEach((e) => {
+    Object.keys(veins[e]).forEach((f) => {
+      let items = [];
+      veins[e][f].forEach((c) => {
+        items.push(CreateItem(c, 25));
+      });
+      SifterRecipe(IDSpecial(f), items);
+    });
+  });
+  
+  // rock breaker recipe generator
+  breakerblocks.forEach((e) => {
+    RockBreakerRecipe(e);
+  });
+
+  let results = {
+    end: {
+      end_stone: Object.keys(veins.end_ore_vein).map((e) =>
+        CreateItem(IDSpecial(e), 25)
+      ),
+    },
+    nether: {
+      blackstone: Object.keys(veins.nether_ore_vein)
+        .slice(0, 6)
+        .map((e) => CreateItem(IDSpecial(e), 25)),
+      basalt: Object.keys(veins.nether_ore_vein)
+        .slice(6)
+        .map((e) => CreateItem(IDSpecial(e), 25)),
+    },
+    deep: {
+      tuff: Object.keys(veins.deep_ore_vein)
+        .slice(0, 6)
+        .map((e) => CreateItem(IDSpecial(e), 25)),
+      cobbled_deepslate: Object.keys(veins.deep_ore_vein)
+        .slice(6)
+        .map((e) => CreateItem(IDSpecial(e), 25)),
+    },
+    stone: {
+      cobble: Object.keys(veins.stone_ore_vein)
+        .slice(0, 6)
+        .map((e) => CreateItem(IDSpecial(e), 25)),
+      gravel: Object.keys(veins.stone_ore_vein)
+        .slice(6, 12)
+        .map((e) => CreateItem(IDSpecial(e), 25)),
+      sand: Object.keys(veins.stone_ore_vein)
+        .slice(12)
+        .map((e) => CreateItem(IDSpecial(e), 25)),
+    },
+  };
+
+  //vein input shifter recipe generator
+  Object.keys(results).forEach((e) => {
+    Object.keys(results[e]).forEach((b) => {
+      SifterRecipe("minecraft:" + b, results[e][b]);
+    });
+  });
+
+  //lang file content
+  let langFile = { "gtnihilo.tab": "GregTech Sifter Expansion" };
+  Object.keys(veins).forEach((e) => {
+    //vein type         -> end_ore_vein
+    Object.keys(veins[e]).forEach((f, i) => {
+      //vein name       -> bauxite_vein_end
+      langFile["item.gtnihilo." + f] = formatNameDataWithSpaces(f);
+
+      io.mkFile("./assets/gtnihilo/models/item/" + f + ".json", {
+        loader: "neoforge:item_layers",
+        parent: "minecraft:item/generated",
+        textures: {
+          layer0: "gtceu:item/material_sets/dull/gem",
+          layer1: "gtceu:item/material_sets/dull/dust_pure_overlay",
+        },
+        neoforge_data: {
+          layers: {
+            0: { color: colors[i] },
+          },
+        },
+      });
+    });
+  });
+  //lang file writer
+  io.mkFile("./assets/gtnihilo/lang/en_us.json", langFile);
+}
---
+++ gregConst.mjs
@@ -0,0 +1,277 @@
+export const files = {
+  assets: {
+    gtnihilo: {
+      models: {
+        item: null,
+      },
+      lang: null,
+    },
+  },
+  data: {
+    gtnihilo: {
+      recipe: {
+        rock_breaker: null,
+        sifter: null,
+      },
+    },
+    gtceu: {
+      recipe: {
+        rock_breaker: null,
+      },
+    },
+  },
+};
+
+export const veins = {
+  end_ore_vein: {
+    bauxite_vein_end: [
+      "gtceu:raw_bauxite",
+      "gtceu:raw_ilmenite",
+      "gtceu:raw_aluminium",
+    ],
+    magnetite_vein_end: [
+      "gtceu:raw_magnetite",
+      "gtceu:raw_vanadium_magnetite",
+      "gtceu:raw_chromite",
+      "minecraft:raw_gold",
+    ],
+    naquadah_vein: ["gtceu:raw_naquadah", "gtceu:raw_plutonium_239"],
+    pitchblende_vein: ["gtceu:raw_pitchblende", "gtceu:raw_uraninite"],
+    scheelite_vein: [
+      "gtceu:raw_scheelite",
+      "gtceu:raw_tungstate",
+      "gtceu:raw_lithium",
+    ],
+    sheldonite_vein: [
+      "gtceu:raw_bornite",
+      "gtceu:raw_cooperite",
+      "gtceu:raw_platinum",
+      "gtceu:raw_palladium",
+    ],
+  },
+
+  nether_ore_vein: {
+    banded_iron_vein: [
+      "gtceu:raw_goethite",
+      "gtceu:raw_yellow_limonite",
+      "gtceu:raw_hematite",
+      "minecraft:raw_gold",
+    ],
+    beryllium_vein: ["gtceu:raw_beryllium", "gtceu:raw_emerald"],
+    certus_quartz_vein: [
+      "gtceu:raw_quartzite",
+      "gtceu:raw_certus_quartz",
+      "gtceu:raw_barite",
+    ],
+    manganese_vein: [
+      "gtceu:raw_grossular",
+      "gtceu:raw_pyrolusite",
+      "gtceu:raw_tantalite",
+    ],
+    molybdenum_vein: [
+      "gtceu:raw_wulfenite",
+      "gtceu:raw_molybdenite",
+      "gtceu:raw_molybdenum",
+      "gtceu:raw_powellite",
+    ],
+    monazite_vein: [
+      "gtceu:raw_bastnasite",
+      "gtceu:raw_monazite",
+      "gtceu:raw_neodymium",
+    ],
+    nether_quartz_vein: ["gtceu:raw_nether_quartz", "gtceu:raw_quartzite"],
+    redstone_vein: [
+      "gtceu:raw_redstone",
+      "gtceu:raw_ruby",
+      "gtceu:raw_cinnabar",
+    ],
+    saltpeter_vein: [
+      "gtceu:raw_saltpeter",
+      "gtceu:raw_diatomite",
+      "gtceu:raw_electrotine",
+      "gtceu:raw_alunite",
+    ],
+    sulfur_vein: [
+      "gtceu:raw_sulfur",
+      "gtceu:raw_pyrite",
+      "gtceu:raw_sphalerite",
+    ],
+    tetrahedrite_vein: [
+      "gtceu:raw_tetrahedrite",
+      "minecraft:raw_copper",
+      "gtceu:raw_stibnite",
+    ],
+    topaz_vein: [
+      "gtceu:raw_blue_topaz",
+      "gtceu:raw_topaz",
+      "gtceu:raw_chalcocite",
+      "gtceu:raw_bornite",
+    ],
+  },
+
+  stone_ore_vein: {
+    apatite_vein: [
+      "gtceu:raw_apatite",
+      "gtceu:raw_tricalcium_phosphate",
+      "gtceu:raw_pyrochlore",
+    ],
+    cassiterite_vein: ["gtceu:raw_tin", "gtceu:raw_cassiterite"],
+    coal_vein: ["gtceu:raw_coal"],
+    copper_tin_vein: [
+      "gtceu:raw_chalcopyrite",
+      "gtceu:raw_zeolite",
+      "gtceu:raw_cassiterite",
+      "gtceu:raw_realgar",
+    ],
+    galena_vein: ["gtceu:raw_galena", "gtceu:raw_silver", "gtceu:raw_lead"],
+    garnet_tin_vein: [
+      "gtceu:raw_cassiterite_sand",
+      "gtceu:raw_garnet_sand",
+      "gtceu:raw_asbestos",
+      "gtceu:raw_diatomite",
+    ],
+    garnet_vein: [
+      "gtceu:raw_red_garnet",
+      "gtceu:raw_yellow_garnet",
+      "gtceu:raw_amethyst",
+      "gtceu:raw_opal",
+    ],
+    iron_vein: [
+      "gtceu:raw_goethite",
+      "gtceu:raw_yellow_limonite",
+      "gtceu:raw_hematite",
+      "gtceu:raw_malachite",
+    ],
+    lubricant_vein: [
+      "gtceu:raw_soapstone",
+      "gtceu:raw_talc",
+      "gtceu:raw_glauconite_sand",
+      "gtceu:raw_pentlandite",
+    ],
+    magnetite_vein_ow: [
+      "gtceu:raw_magnetite",
+      "gtceu:raw_vanadium_magnetite",
+      "minecraft:raw_gold",
+    ],
+    mineral_sand_vein: [
+      "gtceu:raw_basaltic_mineral_sand",
+      "gtceu:raw_granitic_mineral_sand",
+      "gtceu:raw_fullers_earth",
+      "gtceu:raw_gypsum",
+    ],
+    nickel_vein: [
+      "gtceu:raw_garnierite",
+      "gtceu:raw_nickel",
+      "gtceu:raw_cobaltite",
+      "gtceu:raw_pentlandite",
+    ],
+    salts_vein: [
+      "gtceu:raw_rock_salt",
+      "gtceu:raw_salt",
+      "gtceu:raw_lepidolite",
+      "gtceu:raw_spodumene",
+    ],
+    oilsands_vein: ["gtceu:raw_oilsands"],
+  },
+
+  deep_ore_vein: {
+    copper_vein: [
+      "gtceu:raw_chalcopyrite",
+      "minecraft:raw_iron",
+      "gtceu:raw_pyrite",
+      "minecraft:raw_copper",
+    ],
+    diamond_vein: ["gtceu:raw_graphite", "gtceu:raw_diamond", "gtceu:raw_coal"],
+    lapis_vein: [
+      "gtceu:raw_lazurite",
+      "gtceu:raw_sodalite",
+      "gtceu:raw_lapis",
+      "gtceu:raw_calcite",
+    ],
+    manganese_vein_ow: [
+      "gtceu:raw_grossular",
+      "gtceu:raw_spessartine",
+      "gtceu:raw_pyrolusite",
+      "gtceu:raw_tantalite",
+    ],
+    mica_vein: [
+      "gtceu:raw_kyanite",
+      "gtceu:raw_mica",
+      "gtceu:raw_bauxite",
+      "gtceu:raw_pollucite",
+    ],
+    olivine_vein: [
+      "gtceu:raw_bentonite",
+      "gtceu:raw_magnetite",
+      "gtceu:raw_olivine",
+      "gtceu:raw_glauconite_sand",
+    ],
+    redstone_vein_ow: [
+      "gtceu:raw_redstone",
+      "gtceu:raw_ruby",
+      "gtceu:raw_cinnabar",
+    ],
+    sapphire_vein: [
+      "gtceu:raw_almandine",
+      "gtceu:raw_pyrope",
+      "gtceu:raw_sapphire",
+      "gtceu:raw_green_sapphire",
+    ],
+  },
+};
+/*
+      total veins : 40
+      >
+      end : 6           1
+      nether : 12       2
+      overworld : 22 
+      >
+      stone : 14      3
+      deep : 8        2
+      */
+export const breakerblocks = ["minecraft:end_stone", "minecraft:tuff","minecraft:cobbled_deepslate"];
+
+export const breakerReplace = ["blackstone","basalt","cobblestone"]
+
+export const colors = [
+  "#FF5733",
+  "#33FF57",
+  "#3357FF",
+  "#F1C40F",
+  "#8E44AD",
+  "#1ABC9C",
+  "#2ECC71",
+  "#3498DB",
+  "#E74C3C",
+  "#9B59B6",
+  "#34495E",
+  "#16A085",
+  "#27AE60",
+  "#2980B9",
+  "#C0392B",
+  "#D35400",
+  "#7D3C98",
+  "#2980B9",
+  "#F39C12",
+  "#BDC3C7",
+  "#2C3E50",
+  "#7F8C8D",
+  "#95A5A6",
+  "#ECF0F1",
+  "#1F618D",
+  "#F8C471",
+  "#D5F5E3",
+  "#D98880",
+  "#AED6F1",
+  "#A9DFBF",
+  "#BB8FCE",
+  "#F5B041",
+  "#F7DC6F",
+  "#52BE80",
+  "#5DADE2",
+  "#58D68D",
+  "#A04000",
+  "#A569BD",
+  "#7FB3D5",
+  "#ABEBC6",
+];
