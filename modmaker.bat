a1a36c18a41d57e8efe046d38c4f2a9f2a0b4da2
DevDyna <andrearisorti2003@gmail.com>
2025-01-05 16:52:03 +0100

    Update modmaker.bat

--- modmaker.bat
+++ modmaker.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Creating JAR File
-jar cf "NumismaticBounties-universal.jar" data META-INF pack.mcmeta pack.png assets
+jar cf "NumismaticBounties-universal.jar" data META-INF pack.mcmeta pack.png
 ECHO JAR Created!
 TIMEOUT /NOBREAK /T 1 > nul
