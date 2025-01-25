e7c3f7aafba2ef36955fd1819d0cfcade6aa553d
DevDyna <andrearisorti2003@gmail.com>
2025-01-25 14:45:55 +0100

    Delete packager.bat

--- packager.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-echo compressing all recipes , keep on mind that require a few of seconds!
-IF EXIST "ChippedExpress-universal.jar" DEL "ChippedExpress-universal.jar"
-jar cf "ChippedExpress-universal.jar" data META-INF pack.mcmeta pack.png fabric.mod.json
