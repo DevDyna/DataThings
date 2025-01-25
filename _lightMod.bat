cb19679aa70362bce006473d28ea9cf4f3a37f4c
DevDyna <andrearisorti2003@gmail.com>
2025-01-25 23:21:39 +0100

    Delete _lightMod.bat

--- _lightMod.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-ECHO Creating JAR File
-echo compressing all recipes , keep on mind that require a few of seconds!
-jar cf "ChippedExpress-universal-fabric.jar" data pack.mcmeta pack.png fabric.mod.json
-ECHO JAR Created!
-TIMEOUT /NOBREAK /T 1 > nul
