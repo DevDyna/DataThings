a3de84eed7e2dd86d11526fd6973c977d02008f1
DevDyna <andrearisorti2003@gmail.com>
2025-01-25 14:48:17 +0100

    Update _lightMod.bat

--- _lightMod.bat
+++ _lightMod.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 ECHO Creating JAR File
 echo compressing all recipes , keep on mind that require a few of seconds!
-jar cf "ChippedExpress-universal.jar" data pack.mcmeta pack.png fabric.mod.json
+jar cf "ChippedExpress-universal-fabric.jar" data pack.mcmeta pack.png fabric.mod.json
 ECHO JAR Created!
 TIMEOUT /NOBREAK /T 1 > nul
