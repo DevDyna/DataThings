736c86084f6d1b88f5f7fc533f616ed449c474cf
DevDyna <andrearisorti2003@gmail.com>
2025-03-04 19:33:05 +0100

    Create _demo.mjs

---
+++ parts/compat/_demo.mjs
@@ -0,0 +1,46 @@
+import * as io from "../../methods/io.mjs";
+import * as core from "../core.mjs";
+
+export const modid = "dirt";
+export const modid_obj = modid + "_obj";
+export const modid_rew = modid + "_rew";
+export const modid_any = modid + "_any";
+
+export async function decree() {
+  io.mkFile(core.bounty_decrees_url + modid + core.the_end, {
+    requires: [modid],
+    objectives: [core.treasures_id, core.resources_id, modid_obj, modid_any],
+    rewards: [core.decree_id, core.rarities_id, modid_rew, modid_any],
+  });
+  core.decor_out(modid);
+}
+
+export async function pools() {
+  io.mkFile(core.bounty_pools_url + modid_obj + core.the_end, {
+    requires: [modid],
+    content: core.contentCreator(modid, { min: 1, max: 1 }, 1000, [
+      "minecraft:stone",
+    ]),
+  });
+  core.decor_out(modid_obj);
+
+  io.mkFile(core.bounty_pools_url + modid_rew + core.the_end, {
+    requires: [modid],
+    content: core.contentCreator(modid, { min: 1, max: 1 }, 1000, [
+      "minecraft:stone",
+    ]),
+  });
+  core.decor_out(modid_rew);
+
+  io.mkFile(core.bounty_pools_url + modid_any + core.the_end, {
+    requires: [modid],
+    content: core.contentCreator(modid, { min: 1, max: 1 }, 1000, [
+      "minecraft:stone",
+    ]),
+  });
+  core.decor_out(modid_any);
+}
+
+//require to be added to:
+// - ../lang.mjs
+// - ../../execute.mjs
