76c1516096619b5c0b67697968878c2f8cfbdcc7
DevDyna <andrearisorti2003@gmail.com>
2025-03-05 15:16:06 +0100

    Create loottables.mjs

---
+++ parts/loottables.mjs
@@ -0,0 +1,43 @@
+import * as core from "./core.mjs";
+import * as io from "../methods/io.mjs";
+
+export const music_discs = [
+  "music_disc_11",
+  "music_disc_13",
+  "music_disc_far",
+  "music_disc_blocks",
+  "music_disc_cat",
+  "music_disc_chirp",
+  "music_disc_mall",
+  "music_disc_mellohi",
+  "music_disc_stal",
+  "music_disc_strad",
+  "music_disc_wait",
+  "music_disc_ward",
+];
+
+export async function makeLootTables() {
+  let entries = [];
+
+  music_discs.forEach((e) => {
+    entries.push({
+      type: "minecraft:item",
+      name: "minecraft:" + e,
+    });
+  });
+
+  io.mkFile(
+    "./data/extrabounties/loot_tables/entities/creeper_music_discs" +
+      core.the_end,
+    {
+      type: "minecraft:entity",
+      pools: [
+        {
+          rolls: 1,
+          entries: entries,
+        },
+      ],
+    }
+  );
+  core.decor_out("creeper_discs");
+}
