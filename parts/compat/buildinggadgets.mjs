b5cad5aa6c748413cf3c399595a373e09785e023
DevDyna <andrearisorti2003@gmail.com>
2025-04-10 22:46:46 +0200

    Update buildinggadgets.mjs

--- parts/compat/buildinggadgets.mjs
+++ parts/compat/buildinggadgets.mjs
@@ -2,7 +2,8 @@ import * as io from "../../methods/io.mjs";
 import * as core from "../core.mjs";
 export const modid = "buildinggadgets2";
 
-
+export const langkeys = [modid];
+export const displayname = ["BuildingDireThings"];
 
 export async function decree() {
   io.mkFile(core.bounty_decrees_url + modid + core.the_end, {
