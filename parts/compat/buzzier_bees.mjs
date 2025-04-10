8d6d24b8d5907152c3ddc2a1a76d6476e5728024
DevDyna <andrearisorti2003@gmail.com>
2025-04-10 16:58:45 +0200

    Update buzzier_bees.mjs

--- parts/compat/buzzier_bees.mjs
+++ parts/compat/buzzier_bees.mjs
@@ -16,16 +16,31 @@ export async function decree() {
 }
 
 export async function pools() {
-
   io.mkFile(core.bounty_pools_url + modid_rew + core.the_end, {
     requires: [modid],
-    content: core.contentCreator( { min: 1, max: 8 }, 1000, ['buzzier_bees:honey_lamp', 'buzzier_bees:four_leaf_clover', 'buzzier_bees:honeycomb_banner_pattern']),
+    content: core.contentCreator({ min: 1, max: 8 }, 1000, [
+      "buzzier_bees:honey_lamp",
+      "buzzier_bees:four_leaf_clover",
+      "buzzier_bees:honeycomb_banner_pattern",
+    ]),
   });
   core.decor_out(modid_rew);
 
   io.mkFile(core.bounty_pools_url + modid_any + core.the_end, {
     requires: [modid],
-    content: core.contentCreator( { min: 1, max: 8 }, 1000, ['buzzier_bees:endermite_bottle', 'minecraft:honey_bottle', 'minecraft:honeycomb', 'minecraft:honeycomb_block', 'buzzier_bees:glazed_porkchop', 'buzzier_bees:honey_bread', 'buzzier_bees:honey_apple', 'minecraft:honey_block', 'minecraft:sugar', 'buzzier_bees:bee_bottle', 'buzzier_bees:silverfish_bottle']),
+    content: core.contentCreator({ min: 1, max: 8 }, 1000, [
+      "buzzier_bees:endermite_bottle",
+      "minecraft:honey_bottle",
+      "minecraft:honeycomb",
+      "minecraft:honeycomb_block",
+      "buzzier_bees:glazed_porkchop",
+      "buzzier_bees:honey_bread",
+      "buzzier_bees:honey_apple",
+      "minecraft:honey_block",
+      "minecraft:sugar",
+      "buzzier_bees:bee_bottle",
+      "buzzier_bees:silverfish_bottle",
+    ]),
   });
   core.decor_out(modid_any);
 }
