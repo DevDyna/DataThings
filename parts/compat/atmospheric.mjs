2cae4707999314bd4a1b945bfcd3273938dc6682
DevDyna <andrearisorti2003@gmail.com>
2025-04-10 22:46:36 +0200

    Update atmospheric.mjs

--- parts/compat/atmospheric.mjs
+++ parts/compat/atmospheric.mjs
@@ -6,6 +6,9 @@ export const modid = "atmospheric";
 export const modid_rew = modid + "_rew";
 export const modid_any = modid + "_any";
 
+export const langkeys = [modid];
+export const displayname = ["Encyclopedia of the four seasons"];
+
 export async function decree() {
   io.mkFile(core.bounty_decrees_url + modid + core.the_end, {
     requires: [modid],
