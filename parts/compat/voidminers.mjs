4c170f53e1f618efe2a6cfaac57565be71003c4c
DevDyna <andrearisorti2003@gmail.com>
2025-04-10 22:48:00 +0200

    Update voidminers.mjs

--- parts/compat/voidminers.mjs
+++ parts/compat/voidminers.mjs
@@ -6,6 +6,9 @@ export const modid_obj = modid + "_obj";
 export const modid_rew = modid + "_rew";
 export const modid_any = modid + "_any";
 
+export const langkeys = [modid];
+export const displayname = ["Not Environmental Tech related!"];
+
 export async function decree() {
   io.mkFile(core.bounty_decrees_url + modid + core.the_end, {
     requires: [modid],
