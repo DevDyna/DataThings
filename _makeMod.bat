49e1973e0557af1959746ee5b7b2bceeddef794f
DevDyna <andrearisorti2003@gmail.com>
2025-04-27 13:12:52 +0200

    Update _makeMod.bat

--- _makeMod.bat
+++ _makeMod.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Creating JAR File
-jar cf "Create : Numismatic Bounties-universal.jar" data pack.mcmeta pack.png META-INF fabric.mod.json
+jar cf "Create-Numismatic Bounties-universal.jar" data pack.mcmeta pack.png META-INF fabric.mod.json
 ECHO JAR Created!
 TIMEOUT /NOBREAK /T 1 > nul
